98cf9e4868515356a37b5c51cb7a937e3c09a170
KrzysztofKmiecik1975 <krzysztofkmiecik1975@gmail.com>
2022-12-17 16:13:40 +0100

    init commit

---
+++ src/main/java/pl/kmiecik/springliquibasetestcontainersdemo/SpringLiquibaseTestcontainersDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.kmiecik.springliquibasetestcontainersdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLiquibaseTestcontainersDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLiquibaseTestcontainersDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kmiecik/springliquibasetestcontainersdemo/SpringLiquibaseTestcontainersDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kmiecik.springliquibasetestcontainersdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLiquibaseTestcontainersDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
