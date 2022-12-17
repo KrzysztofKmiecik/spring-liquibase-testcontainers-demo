ef6505295f99367feb0a7d3ecc990ed2682067a4
KrzysztofKmiecik1975 <krzysztofkmiecik1975@gmail.com>
2022-12-18 00:39:18 +0100

    liquibase with test

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-dev 
+dev
 docker run --name postgresql-dev -p 5433:5432 -e POSTGRES_PASSWORD=postgres -d postgres
 
+test
+docker run --name postgresql-test -p 5434:5432 -e POSTGRES_PASSWORD=postgres -d postgres
+
 prod
-docker run --name postgresql-prod -p 5434:5432 -e POSTGRES_PASSWORD=postgres -d postgres
+docker run --name postgresql-prod -p 5435:5432 -e POSTGRES_PASSWORD=postgres -d postgres
+
--- src/test/java/pl/kmiecik/springliquibasetestcontainersdemo/controller/PersonControllerIT.java
+++ src/test/java/pl/kmiecik/springliquibasetestcontainersdemo/controller/PersonControllerIT.java
@@ -4,6 +4,8 @@ package pl.kmiecik.springliquibasetestcontainersdemo.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -11,8 +13,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
+@SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
-public class PersonControllerIT extends BaseIT {
+public class PersonControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
