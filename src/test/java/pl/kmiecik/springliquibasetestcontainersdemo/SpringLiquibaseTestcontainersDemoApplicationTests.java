59994c6f9ad5fecf19b6c49f87ba53d59880d5d6
KrzysztofKmiecik1975 <krzysztofkmiecik1975@gmail.com>
2022-12-18 01:00:08 +0100

    disable test

--- src/test/java/pl/kmiecik/springliquibasetestcontainersdemo/SpringLiquibaseTestcontainersDemoApplicationTests.java
+++ src/test/java/pl/kmiecik/springliquibasetestcontainersdemo/SpringLiquibaseTestcontainersDemoApplicationTests.java
@@ -1,7 +1,11 @@
 package pl.kmiecik.springliquibasetestcontainersdemo;
 
+import org.junit.jupiter.api.Test;
+
+//@SpringBootTest
 class SpringLiquibaseTestcontainersDemoApplicationTests {
 
+    @Test
     void contextLoads() {
 
     }
